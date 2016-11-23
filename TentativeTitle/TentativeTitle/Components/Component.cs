0361c29d3e3f59a75ac02b3aa96773a3d5d5ea68
HiddenDeathOrb <hiddendeathorb@gmail.com>
2016-11-23 14:33:22 -0700

    Added component system

---
+++ TentativeTitle/TentativeTitle/Components/Component.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TentativeTitle.Components
+{
+    class Component
+    {
+        public string _name { get; }
+        public string _description { get; } = "A component.";
+        public bool _isEnabled { get; set; } = true;
+        public Entity _owner;
+
+        public Component(string name)
+        {
+            _name = name;
+        }
+        public Component(string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+        public Component GetSiblingComponent<T>()
+        {
+            return _owner.GetComponent<T>();
+        }
+
+        virtual public void Load()
+        { }
+        virtual public void Update(GameTime time)
+        { }
+
+        public override string ToString()
+        {
+            return _name + " - " + _description;
+        }
+    }
+}
---
+++ TentativeTitle/TentativeTitle/Components/Entity.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TentativeTitle.Components
+{
+    class GameTime { }
+    class Entity
+    {
+        protected const int MAX_COMPONENTS = 20;
+        protected Component[] _components;
+        protected int _componentIndex = 0;
+
+        public string _name { get;}
+
+        public Entity(string name)
+        {
+            _components = new Component[MAX_COMPONENTS];
+            _name = name;
+        }
+
+        public bool AddComponent(Component component)
+        {
+            for (int i = 0; i < _componentIndex; i++)
+            {
+                if (_components[i].GetType() == component.GetType())
+                {
+                    Console.Write("Entity::AddComponents - Entity already contains a component of that type! Could not add [" + component._name + "] " + component.GetType().ToString() + "\n");
+                    return false;
+                }
+                else if (_components[i]._name == component._name)
+                {
+                    Console.Write("Entity::AddComponents - Entity already contains a component with that name! Could not add [" + component._name + "] " + component.GetType().ToString() + "\n");
+                    return false;
+                }
+            }
+
+            if (_componentIndex == MAX_COMPONENTS)
+            {
+                Console.Write("Entity::AddComponents - Entity has reached maximum amount of components! Could not add [" + component._name + "] " + component.GetType().ToString() + "\n");
+                return false;
+            }
+
+            component._owner = this;
+            _components[_componentIndex] = component;
+            _componentIndex++;
+            return true;
+        }
+
+        public void UpdateComponent(Component component)
+        {
+            for (int i = 0; i < _componentIndex; i++)
+            {
+                if (_components[i].GetType() == component.GetType())
+                {
+                    _components[i] = component;
+                }
+            }
+        }
+
+        public Component GetComponent<T>()
+        {
+            for (int i = 0; i < _componentIndex; i++)
+            {
+                if (_components[i] is T)
+                {
+                    return _components[i];
+                }
+            }
+            Console.Write("Entity::GetComponent - Could not find component (" + typeof(T).ToString() + ")\n");
+            return null;
+        }
+
+        public Component GetComponent(string name)
+        {
+            for (int i = 0; i < _componentIndex; i++)
+            {
+                if (_components[i]._name == name)
+                {
+                    return _components[i];
+                }
+            }
+            Console.Write("Entity::GetComponent - Could not find component with name [" + name + "]!\n");
+            return null;
+        }
+
+        public void Update(GameTime time)
+        {
+            for (int i = 0; i < _componentIndex; i++)
+            {
+                if (_components[i]._isEnabled)
+                {
+                    _components[i].Update(time);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string componentList = "";
+            for (int i = 0; i < _componentIndex; i++)
+            {
+                componentList += _components[i].ToString() + " (" + _components[i].GetType().ToString() + ") \n";
+            }
+
+            return "------ " + _name + " -----\n" +
+                   "------------------------ \n" +
+                   componentList +
+                   "------------------------ \n";
+        }
+    }
+}
