127b51166f5652717fde65a9c40cd0ca863c7c92
HiddenDeathOrb <hiddendeathorb@gmail.com>
2016-11-24 09:49:09 -0700

    ScenePlay now playing music (although it's not a smooth loop... :( )

--- TentativeTitle/TentativeTitle/GameState/ScenePlay.cs
+++ TentativeTitle/TentativeTitle/GameState/ScenePlay.cs
@@ -7,12 +7,13 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.Diagnostics;
+using Microsoft.Xna.Framework.Media;
 
 namespace TentativeTitle.GameState
 {
     class ScenePlay : Scene
     {
-        
+        private Song _song;
         public void Draw(SpriteBatch batch)
         {
         }
@@ -20,11 +21,15 @@ namespace TentativeTitle.GameState
         public void LoadContent(ContentManager content)
         {
             MouseManager.CurrentCursor = MouseManager.Cursor.CROSSHAIR;
+            _song = content.Load<Song>(@"music/AVoid");
+            MediaPlayer.Play(_song);
+            MediaPlayer.IsRepeating = true;
         }
 
         public void UnloadContent()
         {
-
+            MediaPlayer.Stop();
+            _song.Dispose();
         }
 
         public void Update(GameTime gameTime)
